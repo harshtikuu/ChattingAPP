1e50fb550a32c8b6b4669d7e61ba839e7a8f50cf
Harsh Tiku <tiku.harsh@gmail.com>
2017-11-21 02:27:48 +0530

    Add files via upload

---
+++ node2.py
@@ -0,0 +1,46 @@
+import threading
+import socket
+
+
+class chatroom(object):
+	def __init__(self,hostip,sendport):
+		#threading.Thread.__init__(self)
+		self.hostip=hostip
+		self.sendport=sendport
+		self.listenport=12345
+
+	def sendmessage(self):
+		while True:
+			
+			text=input()
+			s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			s.connect(('127.0.0.1',self.sendport))
+			s.send(bytes(text,'utf-8'))
+			s.close()
+
+
+	def receivemessage(self):
+		s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s.bind(('',self.listenport))
+		s.listen(4)
+		while True:
+
+			c,addr=s.accept()
+			msg=c.recv(1024)
+			print('Friend says : {}'.format(msg.decode('utf-8')))
+			c.close()
+
+
+	def run(self):
+			threading.Thread( target= self.receivemessage,args=()).start()
+			self.sendmessage()
+
+
+if __name__ == '__main__':
+	chat=chatroom('127.0.0.1',12346)
+	chat.run()
+
+
+
